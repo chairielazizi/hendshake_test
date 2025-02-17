14daa5ee407ad5ea5607130ff2b75bb80b521017
chairielazizi <azizihr2000@gmail.com>
2025-02-17 20:29:36 +0800

    make header

---
+++ app/(components)/Header.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import React from "react";
+import { HiBookOpen, HiHome } from "react-icons/hi";
+
+const Header = () => {
+  return (
+    <nav className="text-black flex flex-row items-center w-full mx-auto bg-yellow-500 h-10 justify-between">
+      <div className="flex flex-row">
+        <Link href="/">
+          <HiHome className="text-3xl ml-5 cursor-pointer" />
+        </Link>
+        <Link href="/TodoForm">
+          <HiBookOpen className="text-3xl ml-5 cursor-pointer" />
+        </Link>
+      </div>
+      <div className="text-2xl mr-5 font-semibold">Hendshake Todo List</div>
+    </nav>
+  );
+};
+
+export default Header;
